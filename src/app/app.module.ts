bfdebc3a499438be259c01d71f4940900b2cf814
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-02-11 11:33:31 +0100

    use of angular-cli to generate a module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlogpostComponent } from './blogpost/blogpost.component';
+import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogpostComponent,
+    BlogpostListComponent
   ],
   imports: [
     BrowserModule,
